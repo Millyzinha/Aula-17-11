ec974129bc3327530d788475db89edbfdc1426f3
Millyzinha <94566681+Millyzinha@users.noreply.github.com>
2021-11-17 17:33:24 -0300

    Add files via upload

---
+++ documento Kamilly/atvariaveis.js
@@ -0,0 +1,130 @@
+let nome = "Marcy"
+console.log(nome)
+
+let nome2 = "Paula"
+console.log(nome2)
+
+let nome3 = "João"
+console.log(nome3)
+
+console.log ("comidas")
+let comida  = "pera"
+let comida2 = "arroz"
+let comida3 = "lasanha"
+let comida4 = "empadão"
+let comida5 = "carne"
+
+console.log(comida, comida2, comida3, comida4, comida5)
+
+console.log ("nomes")
+let pessoa1 = "Pantera"
+let sobrenome1 = "Negra"
+let pessoa2 = "Carlos" 
+let sobrenome2 = "Eduardo"
+console.log(pessoa1 + " " + sobrenome1)
+console.log(pessoa2 + " " + sobrenome2)
+
+
+let isAtivo = true
+let isAprovado = false
+let isMolhado = true
+let isMaior = 7 > 2
+console.log(isAprovado, isAtivo, isMolhado, isMaior)
+
+
+console.log("Lista de números")
+let n1 = 10
+let n2 = 9
+let n3 = 8
+let n4 = 6
+let n5 = 3
+let n6 = 2
+let n7 = 1
+let n8 = 5
+console.log(n1, n2, n3, n4, n5, n6, n7, n8)
+
+
+const NUMERO1 = 10
+const NUMERO2 = 17
+const NUMERO3 = 26
+const NUMERO4 = 31
+const NUMERO5 = 20
+console.log(NUMERO1, NUMERO2,NUMERO3, NUMERO4, NUMERO5 )
+
+
+
+console.log("Subtração")
+let num1 = 10
+let num2 = 2
+let resultado = num1 - num2
+console.log(num1 + " - " + num2 + " - " + resultado)
+
+console.log("Divisão")
+let di1 = 30
+let di2 = 5
+let resultado2 = di1 / di2
+console.log(di1 + " / " + di2 + " / " + resultado2)
+
+
+
+console.log("Multiplicação")
+let Mul1 = 6
+let Mul2 = 2 
+let resultado3 = Mul1 * Mul2
+console.log(Mul1 + " * " + Mul2 + " * " + resultado3)
+
+
+console.log("resto de divisão")
+let res1 = 5
+let res2 = 2 
+let resultado4 = res1 % res2
+console.log(res1 + " % " + res2 + " % " + resultado4)
+
+
+console.log("Resto de um numero inteiro")
+let numer1 = 16
+let numer2 = 2
+let resultado5 = numer1 % numer2
+console.log(numer1 + " % " + numer2 + " = " + resultado5)
+
+
+console.log("Percentual")
+let nnu1 = 6
+let Percentual = nnu1 * 0.05
+console.log("Valor:" ,nnu1, "5% do valor: ", Percentual.toFixed(2))
+
+
+console.log("produto")
+let pro1 = 15
+let  qnt = 3 
+let total = pro1 * qnt
+console.log("valor", pro1, qnt, "total:", total)
+
+console.log("caracteres")
+let nm1 = "Graziela"
+let nm2 = "Maria"
+let nm3 = "Vitor"
+let nm4 = "Pietro" 
+
+let totalcaracteres = nm1.length + nm2.length + nm3.length + nm4.length
+console.log(nm1, nm1.length)
+console.log(nm2, nm2.length)
+console.log(nm3, nm3.length)
+console.log(nm4, nm4.length)
+console.log("total:", totalcaracteres)
+
+console.log("pontenciação")
+let base = 6
+let expoente = 4
+let rt = base ** expoente
+console.log(rt)
+base = 2
+expoente = 2
+rt = Math.pow(base, expoente)
+console.log(base, expoente, rt)
+
+
+console.log(" raiz quadrada")
+let valor = 30
+let raiz = Math.sqrt(valor)
+console.log("raiz", raiz)
